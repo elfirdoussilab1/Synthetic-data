b51e2ede00e8bda8a6ad94d7e9a7a53d043ccec7
Aymane <afirdoussi123@gmail.com>
2024-07-29 11:29:42 +0400

    implemented risk and accuracy simulations

--- simulate_risk.py
+++ simulate_risk.py
@@ -1,4 +1,4 @@
-# In this file, we will show that theoretical test accuracy matchs well the empirical one
+# In this file, we will show that theoretical test risk matchs well the empirical one
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm.auto import tqdm
@@ -7,7 +7,7 @@ from utils import *
 
 plt.rcParams.update({"text.usetex": True,"font.family": "STIXGeneral"})#,"font.sans-serif": "Helvetica",})
 # Parameters
-n = 5000
+n = 1000
 m = 2000
 p = 500
 mu = 1.5
@@ -24,18 +24,18 @@ means_practice = []
 means_theory = []
 for gamma in tqdm(gammas):
     # Theory
-    means_theory.append(test_accuracy(n, m, p, mu, mu_orth, beta, sigma, epsilon, rho, phi, gamma))
+    means_theory.append(test_risk(n, m, p, mu, mu_orth, beta, sigma, epsilon, rho, phi, gamma))
 
     # Empirical
-    means_practice.append(empirical_accuracy(batch, n, m, p, mu, mu_orth, beta, sigma, epsilon, rho, phi, gamma, data_type = 'synthetic'))
+    means_practice.append(empirical_risk(batch, n, m, p, mu, mu_orth, beta, sigma, epsilon, rho, phi, gamma, data_type = 'synthetic'))
 
 # Plotting results
 fig, ax = plt.subplots()
 ax.semilogx(gammas, means_theory, label = 'Theory', color = 'purple', linewidth = 3)
 ax.scatter(gammas, means_practice, label = 'Simulation', marker = 'D', alpha = .7, color = 'green')
 ax.set_xlabel('$\gamma$')
-ax.set_ylabel('Test Accuracy')
+ax.set_ylabel('Test Risk')
 ax.grid(True)
 ax.legend()
-path = './results-plot/' + f'simulate_accuracy-n-{n}-m-{m}-p-{p}-beta-{beta}-mu-{mu}-mu_orth-{mu_orth}-sigma-{sigma}-epsilon-{epsilon}-rho-{rho}-phi-{phi}.pdf'
+path = './results-plot/' + f'simulate_risk-n-{n}-m-{m}-p-{p}-beta-{beta}-mu-{mu}-mu_orth-{mu_orth}-sigma-{sigma}-epsilon-{epsilon}-rho-{rho}-phi-{phi}.pdf'
 fig.savefig(path, bbox_inches='tight')
